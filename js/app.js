699e9556ded62e0cb003129119f56a968515863d
Franco Iñigo <franco.millo77@gmail.com>
2023-11-30 18:10:00 -0300

    agregamos la logica para eliminar parrafo nuevo

--- js/app.js
+++ js/app.js
@@ -14,37 +14,49 @@ const cambiarTitulo = ()=>{
 }
 
 const verMas = ()=>{
-    console.log("desde la funcion verMas")
-    //crear un parrafo nuevo
-    const parrafoNuevo = document.createElement("p");
-    parrafoNuevo.innerText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, ipsa
-    quam aut odit maxime, eligendi dolor nesciunt pariatur nisi beatae
-    dolore, culpa iste? Quod minus, tenetur incidunt ducimus molestias
-    neque vitae maiores ratione ea perferendis facere nulla, itaque
-    deserunt, totam quas officia. Voluptate cupiditate eveniet, est
-    numquam beatae neque, veritatis debitis ea iusto magni, sapiente totam
-    architecto assumenda. Quasi tempore tenetur, dolor beatae voluptatibus
-    maiores. Unde et, quod minus quam eos debitis sapiente consequatur
-    laudantium explicabo in facilis vitae veniam neque nesciunt animi
-    reprehenderit qui, recusandae quisquam nam tenetur? Iure, ad! Maxime
-    adipisci neque incidunt omnis, rerum corrupti sequi eveniet aspernatur
-    in velit minima, minus quidem iusto ex autem tempora laudantium? Cum
-    inventore laudantium nobis iure, consectetur a, excepturi explicabo
-    aliquid neque est ut recusandae facere reprehenderit dolor possimus
-    ratione expedita tempora tenetur odio, eveniet sint ipsum at? Iusto
-    ducimus maxime ratione perferendis ipsa soluta adipisci facilis
-    quibusdam voluptatibus consequatur.`;
-    parrafoNuevo.className = "lead";
-
-    //traigo mi elemento (nodo) padre
-const seccion = document.querySelector("#contenedorPadre");
-console.log(seccion)
-
-//opciones para agregar un nodo hijo:
-
-//seccion.appendChild(parrafoNuevo);//con este metodo se agrega como ultimo hijo;
-//seccion.prepend(parrafoNuevo);//con este metodo se agrega al principio el hijo;
-seccion.insertBefore(parrafoNuevo, btnVerMas[3]);
+        //traigo mi elemento (nodo) padre
+        const seccion = document.querySelector("#contenedorPadre");
+    if(btnVerMas[3].innerHTML === "Ver mas"){
+        console.log("desde la funcion verMas")
+        //crear un parrafo nuevo
+        const parrafoNuevo = document.createElement("p");
+        parrafoNuevo.innerText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, ipsa
+        quam aut odit maxime, eligendi dolor nesciunt pariatur nisi beatae
+        dolore, culpa iste? Quod minus, tenetur incidunt ducimus molestias
+        neque vitae maiores ratione ea perferendis facere nulla, itaque
+        deserunt, totam quas officia. Voluptate cupiditate eveniet, est
+        numquam beatae neque, veritatis debitis ea iusto magni, sapiente totam
+        architecto assumenda. Quasi tempore tenetur, dolor beatae voluptatibus
+        maiores. Unde et, quod minus quam eos debitis sapiente consequatur
+        laudantium explicabo in facilis vitae veniam neque nesciunt animi
+        reprehenderit qui, recusandae quisquam nam tenetur? Iure, ad! Maxime
+        adipisci neque incidunt omnis, rerum corrupti sequi eveniet aspernatur
+        in velit minima, minus quidem iusto ex autem tempora laudantium? Cum
+        inventore laudantium nobis iure, consectetur a, excepturi explicabo
+        aliquid neque est ut recusandae facere reprehenderit dolor possimus
+        ratione expedita tempora tenetur odio, eveniet sint ipsum at? Iusto
+        ducimus maxime ratione perferendis ipsa soluta adipisci facilis
+        quibusdam voluptatibus consequatur.`;
+        parrafoNuevo.className = "lead";
+    
+    
+    
+    //opciones para agregar un nodo hijo:
+    
+    //seccion.appendChild(parrafoNuevo);//con este metodo se agrega como ultimo hijo;
+    //seccion.prepend(parrafoNuevo);//con este metodo se agrega al principio el hijo;
+    seccion.insertBefore(parrafoNuevo, btnVerMas[3]);
+    
+    btnVerMas[3].innerHTML = "Ocultar";
+    btnVerMas[3].className = "btn btn-danger";
+    
+    }else{
+        console.log(seccion.children[3]);
+        seccion.removeChild(seccion.children[3]);
+        btnVerMas[3].innerHTML = "Ver mas";
+    btnVerMas[3].className = "btn btn-primary";
+    }
+    
 }
 
 const btnVerMas = document.getElementsByTagName("button");
